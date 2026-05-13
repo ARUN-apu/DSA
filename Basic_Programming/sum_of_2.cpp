4c5a03074ff3a58c2335869c3a23a3cbe8d497f8
Arun Senapati <arunsenapati407@gmail.com>
2026-05-13 09:19:18 +0530

    Sum of Two numbers

---
+++ Basic_Programming/sum_of_2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1;
+    int num2;
+    cout << "Enter Two Numbers: ";
+    cin >> num1;
+    cin >> num2;
+    cout << "Sum of Two number is : " << num1 + num2 << endl;
+    return 0;
+}
