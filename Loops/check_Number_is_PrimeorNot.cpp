eba95e53f92a3fadec08452748229ba5561d2338
Arun Senapati <arunsenapati407@gmail.com>
2026-05-14 09:43:38 +0530

    refactor

--- Loops/check_Number_is_PrimeorNot.cpp
+++ Loops/check_Number_is_PrimeorNot.cpp
@@ -7,7 +7,8 @@ int main()
     cout << "Enter the number: ";
     cin >> num;
 
-    for (int i = 2; i < num; i++)
+    // for (int i = 2; i < num; i++)
+    for (int i = 2; i * i <= num; i++)
     {
         if (num % i != 0)
         {
