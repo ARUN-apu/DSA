b28ce668b379809bb34efd038f17e17ef82d8f59
Arun Senapati <arunsenapati407@gmail.com>
2026-05-14 09:31:13 +0530

    Sum of All OddNumbers

---
+++ Loops/SumofAllOddNumbers.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n = 10;
+    int oddSum = 0;
+    // for (int i = 1; i <= n; i++)
+    // {
+    //     if (i % 2 != 0)
+    //     {
+    //         oddSum += i;
+    //     }
+    // }
+
+    // Using While Loop
+    int i = 1;
+    while (i <= n)
+    {
+        if (i % 2 != 0)
+        {
+            oddSum += i;
+        }
+        i++;
+    }
+    cout << "Sum  of All Odd numbers is : " << oddSum << endl;
+    return 0;
+}
