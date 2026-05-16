d4f28e6a87324190be429de73c04a687503ce009
Arun Senapati <arunsenapati407@gmail.com>
2026-05-16 23:56:41 +0530

    Refactor of Triangle Pattern

--- Patterns/TrianglePattern.cpp
+++ Patterns/TrianglePattern.cpp
@@ -6,14 +6,12 @@ int main()
     int n;
     cout << "Enter a number: ";
     cin >> n;
-    int m = 1;
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < m; j++)
+        for (int j = 0; j < i + 1; j++)
         {
             cout << "* ";
         }
-        m++;
         cout << endl;
     }
     return 0;
