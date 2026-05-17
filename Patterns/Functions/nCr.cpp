958e1b1f9cab2fdcfe5d66726c4f07939b4c9bf3
Arun Senapati <arunsenapati407@gmail.com>
2026-05-17 16:29:15 +0530

    Bionomial coefficient

---
+++ Patterns/Functions/nCr.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int factorial(int n)
+{
+    int fact = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        fact *= i;
+    }
+    return fact;
+}
+
+int nCr(int n, int r)
+{
+    int fact_n = factorial(n);
+    int fact_r = factorial(r);
+    int fact_nmr = factorial(n - r);
+
+    int nCr = fact_n / (fact_r * fact_nmr);
+    return nCr;
+}
+int main()
+{
+    int n, r;
+    cout << "Enter n: ";
+    cin >> n;
+    cout << "Enter r: ";
+    cin >> r;
+
+    cout << nCr(n, r);
+
+    return 0;
+}
