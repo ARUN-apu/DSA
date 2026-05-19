774bdf2d2d9f84406ce20c9fb2dc076b5abb7a2d
Arun Senapati <arunsenapati407@gmail.com>
2026-05-19 14:52:28 +0530

    Basic of Arrays

---
+++ Arrays/BasicofArrays.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int size = 5;
+    int marks[size] = {10, 11, 12, 13, 14};
+
+    for (int i = 0; i < size; i++)
+    {
+        cout << marks[i] << endl;
+    }
+}
