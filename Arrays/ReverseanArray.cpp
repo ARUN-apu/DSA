15bf4ebce2ef84b9d2d418e65346f4eab2339926
Arun Senapati <arunsenapati407@gmail.com>
2026-05-20 08:44:09 +0530

    Reverse an Array using 2 pointer Approach

---
+++ Arrays/ReverseanArray.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+void ReverseArray(int arr[], int size)
+{
+    int start = 0;
+    int end = size - 1;
+    while (start <= end)
+    {
+        swap(arr[start], arr[end]);
+        start++;
+        end--;
+    }
+}
+
+int main()
+{
+    int arr[] = {1, 5, 4, 2, 8, 9};
+    int size = sizeof(arr) / sizeof(int);
+
+    ReverseArray(arr, size);
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
